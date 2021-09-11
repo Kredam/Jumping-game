03db712446e4e4b639b1b2517ada44b8791daed7
Kreidli Ádám <akitlosz2000@gmail.com>
2021-09-11 19:05:21 +0200

    increase speed when both objects on screen and when the custom id detected

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
-import os
-import random
-import time
-
+import pygame.event
 from Obstacles import *
-import pygame as py
 from Menu import *
 import Character
-import glob
+from CustomEvents import *
+
 
 # player
 player_size = (75, 150)
 player_coordinates = [0, 575]
-player_name = ""
 player = Character.Character(player_coordinates[0], player_coordinates[1], player_size[0], player_size[1])
 
 # window
@@ -21,14 +17,18 @@ pygame.display.set_caption("The Epic Sword Guy")
 bg = pygame.transform.scale(pygame.image.load(
     os.path.join("assets/background", "Background.png")), window_size)
 
+custom_events = CustomEvents()
+
+
 # menu
-menu = Menu(24)
+menu = Menu(font_size=24)
+
 # obstacles
 lava = Obstacles(window_size[0], 700, 300, 75, 15)
 lava.load_image(os.path.join("assets/Obstacles/lava", "lava0.png"))
-
 spikes = Obstacles(window_size[0], 700, 300, 285, 0)
 spikes.load_image(os.path.join("assets/Obstacles", "spikes.png"))
+
 
 list_of_obstacles = [lava, spikes]
 item = 0
@@ -38,7 +38,12 @@ FPS = 27
 def redraw_window():
     window.blit(bg, (0, 0))
     if menu.game_started:
+        # speed going to be increased when the custom event id detected in the event queue
+        if pygame.event.get(custom_events.INCREASE_SPEED) and list_of_obstacles[0].x + list_of_obstacles[0].width <= window_size[0] and list_of_obstacles[0].x >= 0 and list_of_obstacles[1].x + list_of_obstacles[1].width <= window_size[0] and list_of_obstacles[1].x > 0:
+            list_of_obstacles[0].increase_speed()
+            list_of_obstacles[1].increase_speed()
         menu.back_to_main(window, player, list_of_obstacles)
+        menu.drag_game_over(player, menu, window, window_size)
         key = pygame.key.get_pressed()
         spawn_obstacle()
         player.border_check(window_size[0])
@@ -47,15 +52,11 @@ def redraw_window():
         player.player_animation(window, FPS)
         player.damage_player(list_of_obstacles, 0)
         player.damage_player(list_of_obstacles, 1)
-        menu.draw_text(window, window_size[0] - 200, 50, f"Score = {player.score}")
-        if player.alive is False:
-            window.blit(menu.game_over_png, (300, 85))
-            menu.draw_text(window, window_size[0]-400, window_size[1]-100, "Please insert your Username!")
+        draw_text(window, window_size[0] - 200, 50, f"Score = {player.score}")
     elif menu.leaderBoard_pressed:
         window.fill(0)
         menu.show_leaderboard(window, window_size[0], window_size[1])
         menu.back_to_main(window, player, list_of_obstacles)
-        # menu.show_leaderboard(window, window_size[0], window_size[1])
     else:
         menu.draw_main_menu(window, window_size[0], window_size[1])
 
@@ -63,39 +64,40 @@ def redraw_window():
 
 
 def spawn_obstacle():
-    distance = random.choice([400, 600, 800])
     window.blit(list_of_obstacles[0].image, (list_of_obstacles[0].x, list_of_obstacles[0].y))
     window.blit(list_of_obstacles[1].image, (list_of_obstacles[1].x, list_of_obstacles[1].y))
     if player.alive:
         list_of_obstacles[0].move()
+        # the 2 object spawn is delayed to avoid object overlap
+        if pygame.event.get(custom_events.DELAY_SPAWN) and list_of_obstacles[0].x + list_of_obstacles[0].width < list_of_obstacles[1].x:
+            list_of_obstacles[1].set_velocity(15)
         list_of_obstacles[1].move()
     if list_of_obstacles[0].x + list_of_obstacles[0].width < 0:
         list_of_obstacles[0].x = window_size[0]
     if list_of_obstacles[1].x + list_of_obstacles[1].width < 0:
         list_of_obstacles[1].x = window_size[0]
-    if list_of_obstacles[0].x < distance:
-        list_of_obstacles[1].velocity = 15
+
 
 def main():
     run = True
     clock = pygame.time.Clock()
-    global player_name
 
     while run:
         clock.tick(FPS)
         for evt in pygame.event.get():
             if evt.type == pygame.QUIT:
                 run = False
-            if evt.type == KEYDOWN and menu.game_started and player.alive is False:
+            if evt.type == pygame.KEYDOWN and menu.game_started and player.alive is False:
                 if evt.unicode.isalpha():
-                    player.name += evt.unicode
-                if evt.key == K_BACKSPACE:
+                    player.name += evt.unicoded
+                if evt.key == pygame.K_BACKSPACE:
                     player.name = player.name[:-1]
-                if evt.key == K_RETURN:
+                if evt.key == pygame.K_RETURN:
                     menu.save_player_stats(player)
                     menu.restart_game(player, list_of_obstacles)
-        menu.draw_text(window, window_size[0]-len(player_name)*15, window_size[1], player.name)
+        draw_text(window, window_size[0]-len(player.name)*15, window_size[1], player.name)
         pygame.display.update()
         redraw_window()
+
 
 main()
